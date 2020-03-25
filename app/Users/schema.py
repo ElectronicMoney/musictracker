d32012a1a78c6499fd3dbc57e04b2260f23396b0
Emeka Augustine <cafafanscoders@gmail.com>
2020-03-25 16:54:49 +0800

    Create User app and its schema

---
+++ app/Users/schema.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+import graphene
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+        # If you want to specify the data you want
+        # only_fields = ('id', 'username', 'email', 'password')
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username  = graphene.String(required=True)
+        email     = graphene.String(required=True)
+        password  = graphene.String(required=True)
+
+    def mutate(self, info, **kwargs):
+        username = kwargs.get('username')
+        email = kwargs.get('email')
+        password = kwargs.get('password')
+        # Create user
+        user = get_user_model()(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        # return the created user
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
--- app/app/schema.py
+++ app/app/schema.py
@@ -1,12 +1,22 @@
 from graphene_django import DjangoObjectType
 import graphene
 import Tracks.schema
+import Users.schema
 
-
-class Query(Tracks.schema.Query, graphene.ObjectType):
+# Queries
+class Query(
+    Tracks.schema.Query, 
+    graphene.ObjectType
+):
    pass
 
-class Mutation(Tracks.schema.Mutation, graphene.ObjectType):
+# Mutations
+class Mutation(
+    Users.schema.Mutation, 
+    Tracks.schema.Mutation, 
+    graphene.ObjectType
+):
     pass
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
